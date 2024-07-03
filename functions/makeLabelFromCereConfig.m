be50478b7bdb74bc9c9b92a50f1dca0f64579507
noah <nbryson2000@gmail.com>
2024-07-03 17:25:44 -0500

    data visualization and finished proper epoching with parsing parameters in the epoch datastruct

---
+++ functions/makeLabelFromCereConfig.m
@@ -0,0 +1,9 @@
+function label = makeLabelFromCereConfig(config,pair)
+vals = [config.Config];
+ppair = pair+1;
+ma = str2num(vals{3})/1000;
+pw = str2num(vals{5});
+f = str2num(vals{7});
+label = sprintf("Pair %d-%d, %d ma, \n%d %ss pulsewith, %d Hz", ...
+    pair,ppair,ma,pw,char(181),f);
+end
